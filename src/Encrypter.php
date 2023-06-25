c4795ecf6331d96587d2f813832897b93c47dbb4
Waseem <techdevelopers10@gmail.com>
2023-06-25 23:49:32 +0530

    upadate get method code

--- src/Encrypter.php
+++ src/Encrypter.php
@@ -14,7 +14,7 @@ class Encrypter
      */
     public function encrypt($value)
     {
-        return $this->getPrefix() . '_' . openssl_encrypt($value, getMethod(), $this->getKey(), 0, $iv = '');
+        return $this->getPrefix() . '_' . openssl_encrypt($value, $this->getMethod(), $this->getKey(), 0, $iv = '');
     }
 
     /**
@@ -25,7 +25,7 @@ class Encrypter
     {
         $value = str_replace("{$this->getPrefix()}_",'',$value);
 
-        return openssl_decrypt($value, getMethod(), $this->getKey(), 0, $iv = '');
+        return openssl_decrypt($value, $this->getMethod(), $this->getKey(), 0, $iv = '');
     }
 
 
